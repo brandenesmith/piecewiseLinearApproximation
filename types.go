70b3bb9261ee702b2570dabd3a3c74843352af17
Branden E. Smith <smith.brandene@gmail.com>
2015-11-13 17:12:27 -0600

    piecewiseLinearApproximation Add types.go to hold structs.
    - Added the types.go to include structs for an ordered pair and an
    equation representation that represents each of the pieces of the
    piecewise functions created the piecewiseLinearApproximation Algorithm.

---
+++ types.go
@@ -0,0 +1,14 @@
+package piecewiseLinearApproximation
+
+// Pair represents an ordered pair of
+// 64 bit float values.
+type Pair struct {
+	X, Y float64
+}
+
+// Equation represents an equation (Expression)
+// as a string and a domain interval (Pair)
+type Equation struct {
+	Expression string
+	Interval   Pair
+}
