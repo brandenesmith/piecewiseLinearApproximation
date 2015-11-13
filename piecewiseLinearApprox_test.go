6877314ecb6512ad284d027c957ca4714c99926e
Branden E. Smith <smith.brandene@gmail.com>
2015-11-13 17:04:33 -0600

    piecewiseLinearApproximation Fix indentation
    - Fixed some indentation issues in piecewiseLinearApproximation_test.go

--- piecewiseLinearApprox_test.go
+++ piecewiseLinearApprox_test.go
@@ -17,16 +17,16 @@ var maxTest = [][]float64{
 	{0, 0, 0},
 }
 
-var minTest = [][]float64 {
-    {1, 2, 1},
-    {3, 5, 3},
-    {3, 2, 2},
-    {-1, -2, -2},
-    {7000, 6999, 6999},
-    {2.01, 2.11, 2.01},
-    {1.0001, 1.00, 1.00},
-    {0.001, 0.0009, 0.0009},
-    {0, 0, 0},
+var minTest = [][]float64{
+	{1, 2, 1},
+	{3, 5, 3},
+	{3, 2, 2},
+	{-1, -2, -2},
+	{7000, 6999, 6999},
+	{2.01, 2.11, 2.01},
+	{1.0001, 1.00, 1.00},
+	{0.001, 0.0009, 0.0009},
+	{0, 0, 0},
 }
 
 func TestMax(t *testing.T) {
@@ -39,10 +39,10 @@ func TestMax(t *testing.T) {
 }
 
 func TestMin(t *testing.T) {
-    for i := 0; i < len(minTest); i++ {
-        answer := piecewiseLinearApproximation.Min(minTest[i][0], minTest[i][1])
-        if answer != minTest[i][2] {
-            t.Errorf("Min returned %g expecting %g", answer, minTest[i][2])
-        }
-    }
+	for i := 0; i < len(minTest); i++ {
+		answer := piecewiseLinearApproximation.Min(minTest[i][0], minTest[i][1])
+		if answer != minTest[i][2] {
+			t.Errorf("Min returned %g expecting %g", answer, minTest[i][2])
+		}
+	}
 }
